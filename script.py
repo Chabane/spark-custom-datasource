e1436ba4f7bac760d88eb920e3b0728f1a3f4f8f
Chabane R <chabane.refes@gmail.com>
2018-10-08 15:43:01 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,3 @@
-
 from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
@@ -6,8 +5,11 @@ if __name__ == "__main__":
     spark = SparkSession \
     .builder \
     .appName("spark-custom-datasource") \
+    .master("local[2]") \
+    .config("spark.python.profile", "true") \
     .getOrCreate()
-    df = spark.read.format("CustomDataSource").load()
+    
+    df = spark.read.format("com.evid.CustomDataSource").load()
     df.show()
-   
+  
     spark.sparkContext.show_profiles()
