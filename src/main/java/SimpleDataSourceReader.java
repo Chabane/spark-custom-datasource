fed69cf0c7733d34a60e0fe6e82f8ebccf7fc854
Chabane R <chabane.refes@gmail.com>
2018-10-08 11:48:25 +0200

    Update SimpleDataSourceReader.java

--- src/main/java/SimpleDataSourceReader.java
+++ src/main/java/SimpleDataSourceReader.java
@@ -9,8 +9,19 @@ import java.util.List;
 public class SimpleDataSourceReader implements DataSourceReader {
 
     public StructType readSchema() {
-        StructField[] structFields = new StructField[]{
-                new StructField("value", DataTypes.StringType, true, Metadata.empty())
+         StructField[] structFields = new StructField[]{
+                new StructField("ident", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("type", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("name", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("coordinates", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("elevation_ft", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("continent", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("iso_country", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("iso_region", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("municipality", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("gps_code", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("iata_code", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("local_code", DataTypes.StringType, true, Metadata.empty())
         };
 
         StructType structType = new StructType(structFields);
