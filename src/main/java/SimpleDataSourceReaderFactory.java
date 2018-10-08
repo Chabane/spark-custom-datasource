374fc61d14401904a002371c6d8dc05be92294ad
Chabane R <chabane.refes@gmail.com>
2018-10-08 21:31:22 +0200

    Update SimpleDataSourceReaderFactory.java

--- src/main/java/SimpleDataSourceReaderFactory.java
+++ src/main/java/SimpleDataSourceReaderFactory.java
@@ -6,9 +6,16 @@ import org.apache.spark.sql.sources.v2.reader.DataReaderFactory;
 
 public class SimpleDataSourceReaderFactory implements DataReaderFactory<Row>, DataReader<Row> {
 
-        String[][] airports = {{"00A","heliport","Total Rf Heliport","-74.93360137939453, 40.07080078125","11","NA","US","US-PA","Bensalem","00A","","00A"
-}, {"00AA","small_airport","Aero B Ranch Airport","-101.473911, 38.704022","3435","NA","US","US-KS","Leoti","00AA","","00AA"}};
-        
+        String[][] airports;
+
+        public SimpleDataSourceReaderFactory() {
+            airports = new String[1000000][11];
+            String[] tab = {"00A","heliport","Total Rf Heliport","-74.93360137939453, 40.07080078125","11","NA","US","US-PA","Bensalem","00A","","00A" };
+            for(int i = 0; i < airports.length; i++) {
+                airports[i] = tab;
+            }
+        }
+
         public DataReader createDataReader() {
             return new SimpleDataSourceReaderFactory();
         }
