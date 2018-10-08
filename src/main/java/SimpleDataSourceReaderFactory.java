d948e1f836d356c5bf2a37f4a2fe78ad306a72b5
Chabane R <chabane.refes@gmail.com>
2018-10-08 15:41:49 +0200

    Update SimpleDataSourceReaderFactory.java

--- src/main/java/SimpleDataSourceReaderFactory.java
+++ src/main/java/SimpleDataSourceReaderFactory.java
@@ -1,77 +1,13 @@
+
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.sources.v2.reader.DataReader;
 import org.apache.spark.sql.sources.v2.reader.DataReaderFactory;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import java.util.List;
-import java.util.ArrayList;
-
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 public class SimpleDataSourceReaderFactory implements DataReaderFactory<Row>, DataReader<Row> {
 
-        @Setter
-        @Getter
-        @ToString
-        private class Airport {
-                private String ident; 
-                private String type; 
-                private String name; 
-                private String coordinates; 
-                private String elevationFt; 
-                private String continent; 
-                private String isoCountry; 
-                private String isoRegion; 
-                private String municipality; 
-                private String gpsCode; 
-                private String iataCode; 
-                private String localCode;
-        }
-        
-        List<Airport> airports;
-        
-        public SimpleDataSourceReaderFactory() {
-                 try {
-            Reader reader = Files.newBufferedReader(Paths.get("./csv/airport-codes.csv"));
-            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
-                    .withFirstRecordAsHeader()
-                    .withIgnoreHeaderCase()
-                    .withTrim());
-                    
-                    airports = new ArrayList<>();
-                    
-                    for (CSVRecord csvRecord : csvParser) {
-                        Airport airport = new Airport();
-                        airport.setIdent(csvRecord.get("ident")); 
-                        airport.setType(csvRecord.get("type")); 
-                        airport.setName(csvRecord.get("name")); 
-                        airport.setCoordinates(csvRecord.get("coordinates")); 
-                        airport.setElevationFt(csvRecord.get("elevation_ft")); 
-                        airport.setContinent(csvRecord.get("continent")); 
-                        airport.setIsoCountry(csvRecord.get("iso_country")); 
-                        airport.setIsoRegion(csvRecord.get("iso_region")); 
-                        airport.setMunicipality(csvRecord.get("municipality")); 
-                        airport.setGpsCode(csvRecord.get("gps_code")); 
-                        airport.setIataCode(csvRecord.get("iata_code")); 
-                        airport.setLocalCode(csvRecord.get("local_code"));
-                        airports.add(airport);
-                    }
-                } catch(Exception e) {
-                   System.out.println("Error occured"+e);
-                }
-        }
+        String[][] airports = {{"00A","heliport","Total Rf Heliport","-74.93360137939453, 40.07080078125","11","NA","US","US-PA","Bensalem","00A","","00A"
+}, {"00AA","small_airport","Aero B Ranch Airport","-101.473911, 38.704022","3435","NA","US","US-KS","Leoti","00AA","","00AA"}};
         
         public DataReader createDataReader() {
             return new SimpleDataSourceReaderFactory();
@@ -80,12 +16,13 @@ public class SimpleDataSourceReaderFactory implements DataReaderFactory<Row>, Da
         Integer index = 0;
 
         public boolean next() {
-            return index < airports.size();
+            return index < airports.length;
         }
 
         public Row get() {
-            Airport airport = airports.get(index);
-            Row row = RowFactory.create(airport.getIdent());
+            // Airport airport = airports.get(index);
+            Row row = RowFactory.create(airports[index]);
+            System.out.println("Airport ===> "+ row);
             index = index + 1;
             return row;
         }
