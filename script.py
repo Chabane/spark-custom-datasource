c23d031e8218588122023d82d16d6a89a19ed0ad
Chabane R <chabane.refes@gmail.com>
2018-10-09 07:58:19 +0200

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
 
     spark = SparkSession \
     .builder \
-    .master("local[2]") \
     .config("spark.python.profile", "true") \
     .config("spark.sql.execution.arrow.enabled", "true") \
     .getOrCreate()
